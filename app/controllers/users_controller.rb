a0499e77bbd2e2dfe964e844ae839446dd2bb9a2
Ethan <ethanolivieri56@gmail.com>
2025-03-24 08:49:57 -0400

    Added Users Listing Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,15 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] = "Your account information was updated successfully"
-      redirect_to articles_path
-    else
-      render "edit"
-    end
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
@@ -29,6 +19,20 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was updated successfully"
+      redirect_to articles_path
+    else
+      render "edit"
     end
   end
 

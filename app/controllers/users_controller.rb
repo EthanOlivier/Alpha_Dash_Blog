ded166708dd036b19fbb0d65da9a18d1947d6af0
Ethan <ethanolivieri56@gmail.com>
2025-03-26 13:02:41 -0400

    Added the option to delete a users profile and the associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :edit, :update ]
-  before_action :require_user, only: [ :edit, :update ]
-  before_action :require_same_user, only: [ :edit, :update ]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_user, only: [ :edit, :update, :destroy ]
+  before_action :require_same_user, only: [ :edit, :update, :destroy ]
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Your profile and all associated accounts were successfully deleted"
+    redirect_to root_path, status: :see_other
   end
 
   private

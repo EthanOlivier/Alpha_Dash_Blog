7ba3206c3eecfce2a772bee139085ac64492f2da
Ethan <ethanolivieri56@gmail.com>
2025-03-28 07:41:17 -0400

    Added an Integration Test for an invalid category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -5,11 +5,22 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     get "/categories/new"
     assert :success
     assert_difference("Category.count", 1) do
-      post categories_url, params: { category: { name: "Sports" } }
+      post categories_path, params: { category: { name: "Sports" } }
       assert_response :redirect
     end
     follow_redirect!
     assert_response :success
     assert_match "Sports", @response.body
+  end
+
+  test "Get New Category Form and Reject Invalid Category Submission" do
+    get "/categories/new"
+    assert :success
+    assert_no_difference("Category.count") do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "The following errors prevented the ", @response.body
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
   end
 end
